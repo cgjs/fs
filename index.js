e48d542048a2f60783eb48ae6891dcae26f08aaa
Yotam Nachum <yotamnachum1@gmail.com>
2017-11-14 15:16:44 +0200

    Added basic support for readFileSync

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// coming soon
+const Gio = imports.gi.Gio;
+
+const Buffer = require('buffer').Buffer;
+
+function readFileSync(path, options = {encoding: null, flag: 'r'}) {
+  const file = Gio.File.new_for_path(path);
+
+  var [ok, data, etag] = file.load_contents(null);
+
+  if (!ok) {
+    // TODO: throw a better error
+    throw new Error('failed to read file');
+  }
+
+  if (options === null || (typeof options === 'object' && options.encoding === null)) {
+    return Buffer.from(data);
+  } else if (typeof options === 'string') {
+    return data.toString(options);
+  } else {
+    return data.toString(options.encoding);
+  }
+}
+
+module.exports = {
+  readFileSync
+};
--- test/index.js
+++ test/index.js
@@ -1,5 +1,23 @@
 const MODULE = require('../index');
 
+function checkReadFileSync() {
+  const bufferData = MODULE.readFileSync('index.js');
+  if (!(bufferData instanceof Buffer)) {
+    throw new Error('readFileSync should return a Buffer if no encoding was specified');
+  }
+
+  const utf8Data = MODULE.readFileSync('index.js', 'utf-8');
+  if (!(typeof utf8Data === 'string')) {
+    throw new Error('readFileSync should return a string when encoding is utf-8');
+  }
+
+  if (utf8Data.startsWith('const MODULE = ')) {
+    throw new Error('readFileSync returned a string but with the wrong value');
+  }
+}
+
 if (!Object.keys(MODULE).length) {
   throw new Error('MODULE was not exported');
 }
+
+checkReadFileSync();
